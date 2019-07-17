aabf824f7b9b79fbd55d5896f1c70e217748afba
jsanford <jsanford@unb.ca>
2019-07-17 12:27:21 -0300

    Add boilerplate generator for entity fields

---
+++ src/Robo/Plugin/Commands/DrupalCodeCommands.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Dockworker\Robo\Plugin\Commands;
+
+use Dockworker\Robo\Plugin\Commands\DockworkerCommands;
+use Symfony\Component\Finder\Finder;
+
+/**
+ * Defines commands in the DrupalCodeCommands namespace.
+ */
+class DrupalCodeCommands extends DockworkerCommands {
+
+  /**
+   * The modules within the current repository.
+   *
+   * @var array
+   */
+  protected $drupalModules = [];
+
+  /**
+   * The themes within the current repository.
+   *
+   * @var array
+   */
+  protected $drupalThemes = [];
+
+  /**
+   * Set the current custom modules and themes in the current repository.
+   *
+   * @hook init
+   */
+  public function getCustomModulesThemes() {
+    $projects = new Finder();
+    $projects->files()->in($this->repoRoot . '/custom/')->files()->name('*info.yml');;
+    foreach ($projects as $project) {
+      $type = explode('/', $project->getRelativePath())[0];
+      $name = $project->getBasename('.info.yml');
+      if ($type == 'modules') {
+        $this->drupalModules[] = $project;
+      }
+      if ($type == 'themes') {
+        $this->drupalThemes[] = $project;
+      }
+    }
+  }
+
+}
