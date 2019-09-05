ef6704dc195830aa0fb150e3ddc601b3a3996dd8
jsanford <jsanford@unb.ca>
2019-09-05 13:31:47 -0300

    Add Drupal Sync command

---
+++ src/Robo/Plugin/Commands/DrupalSyncCommands.php
@@ -0,0 +1,363 @@
+<?php
+
+namespace Dockworker\Robo\Plugin\Commands;
+
+use Dockworker\DrupalKubernetesPodTrait;
+use Dockworker\DrupalLocalDockerContainerTrait;
+use Dockworker\Robo\Plugin\Commands\DockworkerApplicationCommands;
+
+/**
+ * Commands to synchronize remote Drupal data into the dockworker instance.
+ */
+class DrupalSyncCommands extends DockworkerApplicationCommands {
+
+  use DrupalKubernetesPodTrait;
+  use DrupalLocalDockerContainerTrait;
+
+  const POD_DATABASE_DUMP_COMPRESSED_FILENAME = 'tmpdb.sql.gz';
+
+  const POD_DATABASE_DUMP_FILENAME = 'tmpdb.sql';
+
+  const POD_FILES_DUMP_FILENAME = 'tmpfiles.tar.gz';
+
+  const POD_FILES_SOURCE = '/app/html/sites/default/files';
+
+  const POD_TEMPORARY_FILE_LOCATION = '/tmp';
+
+  /**
+   * Sets if the remote database be synchronized.
+   *
+   * @var bool
+   */
+  private $drupalRemoteSyncDatabase = TRUE;
+
+  /**
+   * Sets if the remote filesystem should be synchronized.
+   *
+   * @var bool
+   */
+  private $drupalRemoteSyncFiles = TRUE;
+
+  /**
+   * The name of the kubernetes pod to synchronize from.
+   *
+   * @var string
+   */
+  private $drupalRemoteSyncPodName = NULL;
+
+  /**
+   * Synchronize deployed Drupal data into the local dockworker instance.
+   *
+   * @param string $env
+   *   The deploy environment to synchronize from.
+   *
+   * @option bool $no-database
+   *   Do not synchronize the drupal database.
+   * @option bool $no-files
+   *   Do not synchronize the drupal filesystem.
+   *
+   * @command drupal:remote-sync
+   *
+   * @throws \Exception
+   */
+  public function syncDrupalDatabaseFileSystemFromRemote($env, $opts = ['no-database' => FALSE, 'no-files' => FALSE]) {
+    $this->io()->title('Deployed Data Synchronization');
+
+    $this->kubernetesPodNamespace = $env;
+    $this->kubernetesSetupPods($this->instanceName, "Synchronization");
+
+    // All pods should return the same data, so simply use the first.
+    $this->drupalRemoteSyncPodName = $this->kubernetesCurPods[0];
+
+    // Determine operations to perform.
+    $this->drupalRemoteSyncDatabase = !$opts['no-database'];
+    $this->drupalRemoteSyncFiles = !$opts['no-files'];
+
+    // Dump out no-op users.
+    if (!$this->drupalRemoteSyncDatabase && !$this->drupalRemoteSyncFiles) {
+      $this->say('No operations requested. Exiting...');
+      return;
+    }
+
+    // Error Checking.
+    $this->checkRemoteDrush();
+    $this->checkLocalDrush();
+    $this->compareLocalRemoteDrupalVersions();
+
+    if ($this->drupalRemoteSyncDatabase) {
+      $this->syncDrupalDatabaseFromRemote();
+    }
+
+    if ($this->drupalRemoteSyncFiles) {
+      $this->syncDrupalFileSystemFromRemote();
+    }
+
+    if ($this->drupalRemoteSyncDatabase || $this->drupalRemoteSyncFiles) {
+      $this->syncDrupalDatabaseFileSystemCleanup();
+    }
+  }
+
+  /**
+   * Check the remote kubernetes pod responds as expected to drush commands.
+   *
+   * @throws \Exception
+   */
+  private function checkRemoteDrush() {
+    $output = $this->runRemoteDrushCommand('status');
+    $this->checkDrushCommandOutput($output);
+  }
+
+  /**
+   * Run a drush command in the remote kubernetes pod.
+   *
+   * @param string $command
+   *   The drush command to run.
+   *
+   * @return mixed
+   * @throws \Exception
+   */
+  private function runRemoteDrushCommand($command) {
+    return $this->kubernetesPodDrushCommand(
+      $this->drupalRemoteSyncPodName,
+      $this->kubernetesPodNamespace,
+      $command
+    );
+  }
+
+  /**
+   * Compare a drush status command output to determine if Drupal bootstrapped.
+   *
+   * @param array $output
+   *
+   * @throws \Exception
+   */
+  private function checkDrushCommandOutput(array $output) {
+    if (stristr($output[0], '8.')) {
+      return;
+    }
+    throw new \Exception("Remote drush could not bootstrap Drupal instance.");
+  }
+
+  /**
+   * Check the local docker container responds as expected to drush commands.
+   *
+   * @throws \Exception
+   */
+  private function checkLocalDrush() {
+    $output = $this->runRemoteDrushCommand('status');
+    $this->checkDrushCommandOutput($output);
+  }
+
+  /**
+   * Compare the local docker container and emote kubernetes pod drush versions.
+   *
+   * @throws \Exception
+   */
+  private function compareLocalRemoteDrupalVersions() {
+    $remote_output = $this->runRemoteDrushCommand('status');
+    $local_output = $this->runLocalDrushCommand('status');
+    if ($remote_output[0] != $local_output[0]) {
+      $this->say(
+        sprintf(
+          'Your local drupal instance version [%s] does not match the remote [%s].',
+          $local_output[0],
+          $remote_output[0]
+        )
+      );
+      $this->say('To sync, your local instance must be built with the exact same Drupal Core version as remote');
+      throw new \Exception("Remote-local version mismatch.");
+    }
+  }
+
+  /**
+   * Run a drush command in the local docker container.
+   *
+   * @param $command
+   *
+   * @return mixed
+   * @throws \Exception
+   */
+  private function runLocalDrushCommand($command) {
+    return $this->localDockerContainerDrushCommand(
+      $this->instanceName,
+      $command
+    );
+  }
+
+  /**
+   * Synchronize a deployed database into the dockworker instance.
+   *
+   * @throws \Exception
+   */
+  private function syncDrupalDatabaseFromRemote() {
+    $this->io()->newLine();
+    $this->io()->section("Synchronizing Drupal database from [{$this->drupalRemoteSyncPodName}]");
+    $dump_file = self::POD_TEMPORARY_FILE_LOCATION . '/' . self::POD_DATABASE_DUMP_FILENAME;
+    $gz_dump_file = $dump_file . '.gz';
+
+    $this->say("[Remote] (optionally) Removing Drupal database archive file...");
+    $this->runRemoteCommand('rm -f '. $gz_dump_file);
+
+    $this->say("[Remote] Clearing cache {$this->drupalRemoteSyncPodName}...");
+    $this->runRemoteDrushCommand('cr');
+
+    $this->say("[Remote] Dumping Drupal database from {$this->drupalRemoteSyncPodName}...");
+    $this->runRemoteDrushCommand('sql-dump --result-file=' . $dump_file);
+
+    $this->say("[Remote] Compressing Drupal database archive file...");
+    $this->runRemoteCommand('gzip '. $dump_file);
+
+    $this->say("[Remote] Copying Drupal database archive file to Docker Host's temporary directory...");
+    $this->copyRemoteFileToLocal($gz_dump_file, $gz_dump_file);
+
+    $this->say("[Remote] Removing Drupal database archive file...");
+    $this->runRemoteCommand('rm -f '. $gz_dump_file);
+
+    $this->say("[Docker Host] Copying Drupal database archive file to local container...");
+    $this->copyLocalFileToContainer($gz_dump_file, $gz_dump_file);
+
+    $this->say("[Docker Host] Deleting Drupal database archive file...");
+    unlink($gz_dump_file);
+
+    $this->say("[Container] (optionally) Removing Drupal database archive file...");
+    $this->runLocalContainerCommand("rm -f $dump_file");
+
+    $this->say("[Container] Decompressing Drupal database archive file...");
+    $this->runLocalContainerCommand("gunzip $gz_dump_file");
+
+    $this->say("[Container] Importing Drupal database archive file...");
+    $this->runLocalContainerCommand("sh -c \"drush --root=/app/html sql-cli < $dump_file\"");
+
+    $this->say("[Container] Removing Drupal database archive file...");
+    $this->runLocalContainerCommand("rm -f $dump_file");
+
+    $this->say("[Container] Clearing cache...");
+    $this->runLocalDrushCommand("cr");
+  }
+
+  /**
+   * Run a command in the remote kubernetes pod.
+   *
+   * @param string $command
+   *   The command to run.
+   *
+   * @return mixed
+   * @throws \Exception
+   */
+  private function runRemoteCommand($command) {
+    return $this->kubernetesPodExecCommand(
+      $this->drupalRemoteSyncPodName,
+      $this->kubernetesPodNamespace,
+      $command
+    );
+  }
+
+  /**
+   * Copy a remote file to the local (host) filesystem.
+   *
+   * @param string $remote_filename
+   *   The remote filename to copy.
+   * @param $local_filename
+   *   The local filename to write to.
+   *
+   * @return mixed
+   * @throws \Exception
+   */
+  private function copyRemoteFileToLocal($remote_filename, $local_filename) {
+    return $this->kubernetesPodFileCopyCommand(
+      $this->kubernetesPodNamespace,
+      $this->drupalRemoteSyncPodName . ':' . $remote_filename,
+      $local_filename
+    );
+  }
+
+  /**
+   * Copy a local (host) file to the local docker container.
+   *
+   * @param string $local_filename
+   *   The local filename to copy.
+   * @param string $container_filename
+   *   The container filename to write to.
+   *
+   * @return mixed
+   * @throws \Exception
+   */
+  private function copyLocalFileToContainer($local_filename, $container_filename) {
+    return $this->localDockerContainerCopyCommand(
+      $local_filename,
+      $this->instanceName . ':' . $container_filename
+    );
+   }
+
+  /**
+   * Run a command in the local docker container.
+   *
+   * @param string $command
+   *   The command to run.
+   *
+   * @return mixed
+   * @throws \Exception
+   */
+  private function runLocalContainerCommand($command) {
+    return $this->localDockerContainerExecCommand(
+      $this->instanceName,
+      $command
+    );
+  }
+
+  /**
+   * Synchronize a deployed Drupal filesystem into the dockworker instance.
+   *
+   * @throws \Exception
+   */
+  private function syncDrupalFileSystemFromRemote() {
+    $this->io()->newLine();
+    $this->io()->section("Synchronizing Drupal filesystem from [{$this->drupalRemoteSyncPodName}]");
+    $files_dump_name = self::POD_TEMPORARY_FILE_LOCATION . '/' . self::POD_FILES_DUMP_FILENAME;
+
+    $this->say("[Remote] (optionally) Removing Drupal filesystem archive...");
+    $this->runRemoteCommand('rm -f '. $files_dump_name);
+
+    $this->say("[Remote] Creating Drupal filesystem archive...");
+    $this->runRemoteCommand("tar -cvpzf  $files_dump_name " . self::POD_FILES_SOURCE);
+
+    $this->say("[Remote] Copying Drupal filesystem archive to Docker Host's temporary directory...");
+    $this->copyRemoteFileToLocal($files_dump_name, $files_dump_name);
+
+    $this->say("[Remote] Removing Drupal filesystem archive file...");
+    $this->runRemoteCommand('rm -f '. $files_dump_name);
+
+    $this->say("[Docker Host] Copying Drupal filesystem archive to local container...");
+    $this->copyLocalFileToContainer($files_dump_name, $files_dump_name);
+
+    $this->say("[Docker Host] Deleting Drupal filesystem archive...");
+    unlink($files_dump_name);
+
+    $this->say("[Container] Deleting container Drupal filesystem...");
+    $this->runLocalContainerCommand('rm -rf ' . self::POD_FILES_SOURCE);
+
+    $this->say("[Container] Extracting remote Drupal filesystem...");
+    $this->runLocalContainerCommand("tar -xzf $files_dump_name --directory /");
+
+    $this->say("[Container] Removing Drupal filesystem archive file...");
+    $this->runLocalContainerCommand('rm -f '. $files_dump_name);
+
+    $this->say("[Container] Setting overall Drupal filesystem permissions...");
+    $this->runLocalContainerCommand('/scripts/pre-init.d/70_set_permissions.sh');
+  }
+
+  /**
+   * Clean up and post-init tasks for sync.
+   *
+   * @throws \Exception
+   */
+  private function syncDrupalDatabaseFileSystemCleanup() {
+    $this->io()->newLine();
+    $this->io()->section('Cleaning Up');
+    $this->say("Generating New ULI Link...");
+    $this->say(
+      $this->runLocalContainerCommand("/scripts/drupalUli.sh")[0]
+    );
+  }
+
+}
