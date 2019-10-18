cb6fb4b51d71079f0602926e38ca60a7e5bba735
jsanford <jsanford@unb.ca>
2019-10-18 13:10:46 -0300

    Updates to messaging, titling

--- src/Robo/Plugin/Commands/DrupalTestCommands.php
+++ src/Robo/Plugin/Commands/DrupalTestCommands.php
@@ -31,6 +31,7 @@ class DrupalTestCommands extends DockworkerLocalCommands {
    *   The testing command result.
    */
   public function runDrupalBehatTests() {
+    $this->io()->title("Running Behat Tests");
     $this->getLocalRunning();
     return $this->taskDockerExec($this->instanceName)
       ->interactive()
@@ -49,6 +50,7 @@ class DrupalTestCommands extends DockworkerLocalCommands {
    *   The testing command result.
    */
   public function runDrupalUnitTests() {
+    $this->io()->title("Running PHPUnit Tests");
     $this->getLocalRunning();
     return $this->taskDockerExec($this->instanceName)
       ->interactive()
