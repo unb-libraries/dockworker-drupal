6f6ed99f549e612c784f3f1d3efab42bf74c89fd
Jacob Sanford <jsanford@unb.ca>
2020-05-29 12:45:11 -0300

    Add deployment:drupal:cr command

--- src/Robo/Plugin/Commands/DrupalDeploymentCommands.php
+++ src/Robo/Plugin/Commands/DrupalDeploymentCommands.php
@@ -3,6 +3,7 @@
 namespace Dockworker\Robo\Plugin\Commands;
 
 use Dockworker\DockworkerException;
+use Dockworker\DrupalKubernetesPodTrait;
 use Dockworker\KubernetesDeploymentTrait;
 use Dockworker\Robo\Plugin\Commands\DockworkerDeploymentCommands;
 
@@ -10,6 +11,8 @@ use Dockworker\Robo\Plugin\Commands\DockworkerDeploymentCommands;
  * Defines the commands used to interact with a deployed Drupal application.
  */
 class DrupalDeploymentCommands extends DockworkerDeploymentCommands {
+
+  use DrupalKubernetesPodTrait;
 
   /**
    * Provides log checker with ignored log exception items for deployed Drupal.
@@ -21,6 +24,43 @@ class DrupalDeploymentCommands extends DockworkerDeploymentCommands {
       '[notice] Synchronized extensions' => 'Ignore installation of modules that have "error" in their names',
       'config_importer is already importing' => 'Ignore errors when only one pod imports config',
     ];
+  }
+
+  /**
+   * Clears the cache(s) in a remote drupal Deployment.
+   *
+   * @param string $env
+   *   The environment to obtain the logs from.
+   *
+   * @command deployment:drupal:cr
+   * @throws \Exception
+   *
+   * @usage deployment:drupal:cr dev
+   *
+   * @kubectl
+   */
+  public function rebuildRemoteDrupalCache($env) {
+    $this->deploymentCommandInit($this->repoRoot, $env);
+    $this->kubernetesPodNamespace = $this->deploymentK8sNameSpace;
+    $this->kubernetesSetupPods($this->deploymentK8sName, "Rebuild Cache");
+
+    if (!empty($this->kubernetesCurPods)) {
+      $first_pod_id = reset($this->kubernetesCurPods);
+      $this->kubernetesPodExecCommand(
+        $first_pod_id,
+        $env,
+        '/scripts/clearDrupalCache.sh'
+      );
+    }
+    else {
+      throw new DockworkerException(
+        sprintf(
+          self::ERROR_NO_PODS_IN_DEPLOYMENT,
+          $this->deploymentK8sName,
+          $this->deploymentK8sNameSpace
+        )
+      );
+    }
   }
 
 }
