a98fcff7f5524159b778e584b9a9e8db37e410cf
Jacob Sanford <jsanford@unb.ca>
2021-01-06 15:04:56 -0400

    Update D9 audit formalism to match new requirements

--- src/Robo/Plugin/Commands/DrupalUpgrade9AuditCommands.php
+++ src/Robo/Plugin/Commands/DrupalUpgrade9AuditCommands.php
@@ -14,20 +14,30 @@ class DrupalUpgrade9AuditCommands extends DockworkerLocalCommands {
   /**
    * Audits all code in modules/themes against Drupal 9 standards.
    *
-   * @param string $module_theme_root
-   *   The relative path to the module/theme root.
-   *
    * @command validate:drupal:9-upgrade
    * @link https://github.com/JacobSanford/docker-drupal-check
    *
    * @return \Robo\Result
    *   The result of the command.
    */
-  public function auditCode($module_theme_root = '/custom') {
-    return $this->taskDockerRun(self::AUDIT_DOCKER_IMAGE)
-      ->volume($this->repoRoot . $module_theme_root, '/drupal/web/modules/custom')
-      ->exec("/drupal/web/modules/custom")
-      ->run();
+  public function auditCode() {
+    $cmd = $this->taskDockerRun(self::AUDIT_DOCKER_IMAGE)
+      ->volume($this->repoRoot . '/build/composer.json', '/drupal/composer.json');
+
+    $custom_modules_path = "$this->repoRoot/custom/modules";
+    $custom_themes_path = "$this->repoRoot/custom/themes";
+
+    $custom_modules_directories = glob("$custom_modules_path/*" , GLOB_ONLYDIR);
+    if (!empty($custom_modules_directories)) {
+      $cmd->volume($this->repoRoot . '/custom/modules', '/modules');
+    }
+
+    $custom_themes_directories = glob("$custom_themes_path/*" , GLOB_ONLYDIR);
+    if (!empty($custom_themes_directories)) {
+      $cmd->volume($this->repoRoot . '/custom/themes', '/themes');
+    }
+
+    return $cmd->run();
   }
 
 }
