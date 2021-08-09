decfcf578b98e8bcf9f916f6300f15969c614d38
Jacob Sanford <jsanford@unb.ca>
2021-08-09 12:32:01 -0300

    Synchronize webform submitted data

--- src/DrupalDrushSqlDumpTrait.php
+++ src/DrupalDrushSqlDumpTrait.php
@@ -8,7 +8,7 @@ namespace Dockworker;
 trait DrupalDrushSqlDumpTrait {
 
   public function getDrushDumpCommand() {
-    return 'sql-dump --extra-dump=--no-tablespaces --structure-tables-list="accesslog,batch,cache,cache_*,ctools_css_cache,ctools_object_cache,flood,search_*,history,queue,semaphore,sessions,watchdog,webform_submitted_data"';
+    return 'sql-dump --extra-dump=--no-tablespaces --structure-tables-list="accesslog,batch,cache,cache_*,ctools_css_cache,ctools_object_cache,flood,search_*,history,queue,semaphore,sessions,watchdog"';
   }
 
 }
