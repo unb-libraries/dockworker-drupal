565659fd207489f5c1331674fd87bf84dbe3106b
Jacob Sanford <jsanford@unb.ca>
2023-05-29 14:10:49 -0300

    Init repo root before addressing it

--- src/Robo/Plugin/Commands/DrupalGitHookInstallCommands.php
+++ src/Robo/Plugin/Commands/DrupalGitHookInstallCommands.php
@@ -2,20 +2,21 @@
 
 namespace Dockworker\Robo\Plugin\Commands;
 
-use Dockworker\Robo\Plugin\Commands\GitHookInstallCommands;
+use Dockworker\RepoFinder;
 
 /**
  * Provides commands to copy git hooks into a drupal application repository.
  */
-class DrupalGitHookInstallCommands extends GitHookInstallCommands
-{
-    /**
-     * Sets up the required git hooks for dockworker-drupal.
-     *
-     * @hook post-init git:setup-hooks
-     */
-    public function setupDrupalGitHooks(): void
-    {
-        $this->copyGitHookFiles('dockworker-drupal');
-    }
+class DrupalGitHookInstallCommands extends GitHookInstallCommands {
+
+  /**
+   * Sets up the required git hooks for dockworker-drupal.
+   *
+   * @hook post-init git:setup-hooks
+   */
+  public function setupDrupalGitHooks(): void {
+    $this->applicationRoot = RepoFinder::findRepoRoot();
+    $this->copyGitHookFiles('dockworker-drupal');
+  }
+
 }
