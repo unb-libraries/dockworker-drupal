204b96e47433a484f665d6dfd080f4567ee71c4a
Jacob Sanford <jsanford@unb.ca>
2024-07-26 10:20:05 -0300

    Add mailhog commands

---
+++ src/Robo/Plugin/Commands/DrupalMailhogCommands.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace Dockworker\Robo\Plugin\Commands;
+
+use Dockworker\Docker\DockerComposeTrait;
+use Dockworker\DockworkerDrupalCommands;
+use Dockworker\IO\DockworkerIOTrait;
+
+/**
+ * Provides commands for deploying a mailhog service container.
+ */
+class DrupalMailhogCommands extends DockworkerDrupalCommands
+{
+    use DockerComposeTrait;
+    use DockworkerIOTrait;
+
+    /**
+     * Enables Mailhog for his instance.
+     *
+     * @command drupal:mailhog:enable
+     * @aliases mailhog
+     */
+    public function startMailHogContainer(): void {
+        $this->registerDockerCliTool($this->dockworkerIO);
+        if ($this->isMailhogEnabled()) {
+            $this->say('Mailhog is already enabled.');
+            $this->showMailHogUri();
+            return;
+        }
+        $this->startComposeApplication('mailhog');
+        $this->showMailHogUri();
+    }
+
+    /**
+     * Enables Mailhog for his instance.
+     *
+     * @command drupal:mailhog:disable
+     */
+    public function stopMailHogContainer(): void {
+        $this->registerDockerCliTool($this->dockworkerIO);
+        if (!$this->isMailhogEnabled()) {
+            $this->say('Mailhog is already disabled.');
+            return;
+        }
+        $this->stopComposeApplication('mailhog');
+    }
+
+    /**
+     * Shows the Mailhog URI.
+     */
+    protected function showMailHogUri(): void {
+        $this->dockworkerIO->block(
+            sprintf(
+                'Visit the mailhog instance at: http://local-%s:%s/',
+                $this->applicationName,
+                $this->applicationUuid + 1000
+            )
+        );
+    }
+
+    /**
+     * Checks if Mailhog is enabled.
+     *
+     * @return bool
+     *   TRUE if Mailhog is enabled, FALSE otherwise.
+     */
+    protected function isMailhogEnabled(): bool {
+        return $this->composeServiceIsRunning('mailhog');
+    }
+}
